759d641fe5cf8f06736ae0a59651c88530fe1a72
Daniel Bowden <dj.bowden7@gmail.com>
2022-04-16 18:52:38 +0000

    dev container working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PlansModule } from './plans/plans.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PlansModule, SubscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
